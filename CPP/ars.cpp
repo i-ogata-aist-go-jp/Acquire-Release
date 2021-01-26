a51436af2cc77903d1f3b28a444ef8d11a4a24f8
ichiro ogata <i.ogata@aist.go.jp>
2021-01-26 23:14:11 +0900

    std::ref()

--- CPP/ars.cpp
+++ CPP/ars.cpp
@@ -2,24 +2,26 @@
 #include <atomic>
 #include <thread>
 
-std::atomic<int> data(0);
-std::atomic<bool> ready(false);
 
-void producer() {
-	data.store(42, std::memory_order_relaxed); // must be executed before next "store release"
-	ready.store(true, std::memory_order_release);
+void producer(std::atomic<bool>& r, std::atomic<int>& d) {
+	d.store(42, std::memory_order_relaxed); // must be executed before next "store release"
+	r.store(true, std::memory_order_release);
 }
-void consumer() {
-	int r;
-	while (!ready.load(std::memory_order_acquire)) {
+
+void consumer(std::atomic<bool>& r, std::atomic<int>& d) {
+	int reg;
+	while (!r.load(std::memory_order_acquire)) {
 	}
-	r = data.load(std::memory_order_relaxed);  // must be executed after previous "load acquire"
-	printf("data = %d\n",r);
+	reg = d.load(std::memory_order_relaxed);  // must be executed after previous "load acquire"
+	printf("data = %d\n",reg);
 }
 
 int main()
 {
-	std::thread t(producer);
-	consumer();
+	std::atomic<bool> ready(false);
+	std::atomic<int> data(0);
+
+	std::thread t(producer,std::ref(ready),std::ref(data));
+	consumer(std::ref(ready),std::ref(data));
 	t.join();
 }
