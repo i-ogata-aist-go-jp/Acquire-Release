1e4067c7264f8eadfb94c2f51e37afdca1d657e1
ichiro ogata <i.ogata@aist.go.jp>
2021-02-03 18:28:07 +0900

    clean up

--- RUST/fetchadd/target/debug/deps/fetchadd-243b66e034742e57.d
+++
@@ -1,7 +0,0 @@
-/home/ogata/Acquire-Release/RUST/fetchadd/target/debug/deps/fetchadd-243b66e034742e57.s: src/main.rs
-
-/home/ogata/Acquire-Release/RUST/fetchadd/target/debug/deps/fetchadd-243b66e034742e57: src/main.rs
-
-/home/ogata/Acquire-Release/RUST/fetchadd/target/debug/deps/fetchadd-243b66e034742e57.d: src/main.rs
-
-src/main.rs:
--- RUST/fetchadd/target/debug/fetchadd.d
+++
@@ -1,1 +0,0 @@
-/home/ogata/Acquire-Release/RUST/fetchadd/target/debug/fetchadd: /home/ogata/Acquire-Release/RUST/fetchadd/src/main.rs
