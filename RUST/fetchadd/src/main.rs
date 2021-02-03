d262f22b9abcabb971e15df572a24f45ced2e4a6
ichiro ogata <i.ogata@aist.go.jp>
2021-02-03 21:49:47 +0900

    fetchadd

--- RUST/fetchadd/src/main.rs
+++ RUST/fetchadd/src/main.rs
@@ -1,18 +1,22 @@
-use std::sync::Arc;
+use std::time::Instant;
 use std::sync::atomic::{AtomicI64,Ordering};
-use std::thread;
 
-const G: i64 = 1000 * 1000 * 1000;
-const LOOP: i64 =  10 * G ;
+const G: i64 = 1024 * 1024 * 1024;
+const LOOP: i64 =  1 * G ;
+
+fn count_up(k:Ordering) {
+    let c = AtomicI64::new(0);
+    let start = Instant::now();
+    for _ in 0..LOOP {
+        c.fetch_add(1,k);
+        }
+    let end = start.elapsed();
+    println!("{}.{:03}sec elapsed.", end.as_secs(), end.subsec_nanos() / 1_000_000);
+}
 
 fn main() {
-    let c = Arc::new(AtomicI64::new(0));
-    let c_clone = Arc::clone(&c);
-    let t = thread::spawn(move|| {
-        for _ in 0..LOOP {
-            c_clone.fetch_add(1,Ordering::Relaxed);
-            }
-        });
-    t.join().unwrap();
-    eprintln!("counter = {}G", c.load(Ordering::SeqCst) / G);
+    count_up(Ordering::SeqCst);
+    count_up(Ordering::Acquire);
+    count_up(Ordering::Release);
+    count_up(Ordering::Relaxed);
 }
