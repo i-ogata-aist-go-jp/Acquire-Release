8b40bc57e6cf2766a27ddea1c0943577870fb260
ichiro ogata <i.ogata@aist.go.jp>
2021-02-04 11:17:03 +0900

    add macro version

---
+++ RUST/fetchadd/src/macro.rs
@@ -0,0 +1,24 @@
+use std::time::Instant;
+use std::sync::atomic::{AtomicI64,Ordering};
+
+const G: i64 = 1024 * 1024 * 1024;
+const LOOP: i64 =  4 * G ;
+
+macro_rules! count_up {
+    ( $k:expr ) => ({
+     let c = AtomicI64::new(0);
+     let start = Instant::now();
+     for _ in 0..LOOP {
+         c.fetch_add(1,$k);
+         }
+     let end = start.elapsed();
+     println!("{}.{:03}sec elapsed.", end.as_secs(), end.subsec_nanos() / 1_000_000);
+    })
+}
+
+fn main() {
+    count_up!(Ordering::SeqCst);
+    count_up!(Ordering::Acquire);
+    count_up!(Ordering::Release);
+    count_up!(Ordering::Relaxed);
+}
