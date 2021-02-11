975da7d6a1af2444561415bb027a2367f49987e7
ichiro ogata <i.ogata@aist.go.jp>
2021-02-11 10:58:41 +0900

    end signal

--- RUST/ordering/src/main.rs
+++ RUST/ordering/src/main.rs
@@ -1,10 +1,11 @@
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize,Ordering};
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{Sender,channel};
 use std::thread;
 use num_format::{Locale, ToFormattedString};
+use std::time::Instant;
 
-const MAX :u64 = 0x0000_ffff_ffff_ffff_u64;
+const MAX :u64 = 10_000_000_u64;
 
 fn main () {
     let mut detected: u64 = 0;
@@ -19,56 +20,55 @@ fn main () {
     let y_c2 = Arc::clone(&y);
     let r1_c1 = Arc::clone(&r1);
     let r2_c2 = Arc::clone(&r2);
-    // channels
+    // a mpsc channels for start
     let (stx_c1,srx_c1) = channel();
     let (stx_c2,srx_c2) = channel();
-    let (etx_c1,erx_c1) = channel();
-    let (etx_c2,erx_c2) = channel();
+    // a mpsc channel for end
+    let (etx_c1,erx) = channel();
+    let etx_c2 = Sender::clone(&etx_c1);
     // threads
     let t1 = thread::spawn(move|| {
         loop {
-         srx_c1.recv().expect("Could not receive (1) start signal");  // Wait for signal
-         while rand::random::<usize>() % 8 != 0 {}  // Random delay
-         x_c1.store(1,Ordering::Relaxed);
-         let r = y_c1.load(Ordering::Relaxed);
-         r1_c1.store(r,Ordering::Relaxed);
-         etx_c1.send(()).expect("Could not send (1) end signal");  // notify
+            let _ = srx_c1.recv().expect("Could not receive (1) start signal");  // Wait for signal
+            while rand::random::<usize>() % 8 != 0 {}  // Random delay
+            x_c1.store(1,Ordering::Relaxed);
+            let r = y_c1.load(Ordering::Relaxed);
+            r1_c1.store(r,Ordering::Relaxed);
+            etx_c1.send(()).expect("Could not send (1) end signal");  // notify
         }
     });
     let t2 = thread::spawn(move|| {
         loop {
-         srx_c2.recv().expect("Could not receive (2) start signal");  // Wait for signal
-         while rand::random::<usize>() % 8 != 0 {}  // Random delay
-         y_c2.store(1,Ordering::Relaxed);
-         let r = x_c2.load(Ordering::Relaxed);
-         r2_c2.store(r,Ordering::Relaxed);
-         etx_c2.send(()).expect("Could not send (2) end signal");  // notify
+            let _ = srx_c2.recv().expect("Could not receive (2) start signal");  // Wait for signal
+            while rand::random::<usize>() % 8 != 0 {}  // Random delay
+            y_c2.store(1,Ordering::Relaxed);
+            let r = x_c2.load(Ordering::Relaxed);
+            r2_c2.store(r,Ordering::Relaxed);
+            etx_c2.send(()).expect("Could not send (2) end signal");  // notify
         }
-
     });
+    // Start measuring the elapsed time
+    let start = Instant::now();
     // Repeat the experiment ad infinitum
     for iterations in 1..MAX {
         // Reset X and Y
         x.store(0,Ordering::Relaxed);
         y.store(0,Ordering::Relaxed);
-        // Signal both threads
-        stx_c1.send(()).expect("Could not send (1) start signal");
-        stx_c2.send(()).expect("Could not send (2) start signal");
+        // Send start signal
+        stx_c1.send(()).expect("Could not send (2) end signal");  // notify
+        stx_c2.send(()).expect("Could not send (2) end signal");  // notify
         // Wait for both threads
-        erx_c1.recv().expect("Could not receive (1) end signal");
-        erx_c2.recv().expect("Could not receive (2) end signal");
+        erx.recv().expect("Could not receive first end signal");
+        erx.recv().expect("Could not receive second end signal");
         // Check if there was a simultaneous reorder
         if r1.load(Ordering::Relaxed) == 0 && r2.load(Ordering::Relaxed) == 0 {
             detected += 1;
             let sss = iterations.to_formatted_string(&Locale::en);
             println!("{:?} reorders detected after {:?} iterations", detected, sss);
         }
-/* 
-        if iterations % 1_000_000 == 0 {
-           println!(".");
-      }
-*/
     }
+    let end = start.elapsed();
+    println!("{}.{:03}sec elapsed.", end.as_secs(), end.subsec_nanos() / 1_000_000);
     let _res1 = t1.join();
     let _res2 = t2.join();
 }
